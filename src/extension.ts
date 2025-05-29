5a4a98f9e1e933d385f3e0c3f82625859de8c28b
Denys Leon <leonden0046@gmail.com>
2025-05-29 20:02:09 +0300

    user prompt

--- src/extension.ts
+++ src/extension.ts
@@ -4,104 +4,105 @@ import * as path from 'path'
 import * as fs from 'fs'
 
 const openAIClient: OpenAI = new OpenAI({
-	apiKey: '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'
+    apiKey: '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'
 });
 
 let chatGptModel: string = "gpt-4.1-nano";
 
 export async function activate(context: vscode.ExtensionContext) {
 
-	const webviewPanelCommand = vscode.commands.registerCommand('doper.doper', async () => {
-		openReviewPanel(context);
-	});
+    const webviewPanelCommand = vscode.commands.registerCommand('doper.doper', async () => {
+        openReviewPanel(context);
+    });
 
-	const reviewFileCommand = vscode.commands.registerCommand('doper.doper', async (uri: vscode.Uri) => {
-		openReviewPanel(context, uri);
-	});
+    const reviewFileCommand = vscode.commands.registerCommand('doper.doper', async (uri: vscode.Uri) => {
+        openReviewPanel(context, uri);
+    });
 
-	context.subscriptions.push(webviewPanelCommand);
-	context.subscriptions.push(reviewFileCommand);
+    context.subscriptions.push(webviewPanelCommand);
+    context.subscriptions.push(reviewFileCommand);
 }
 
 async function openReviewPanel(context: vscode.ExtensionContext, fileToLoad?: vscode.Uri) {
-	let panelTitle: string = "AI Code Reviewer";
-	let panelIdentifier: string = "AI_Code_Reviewer";
-	let panelViewColumnOptions: vscode.ViewColumn = vscode.ViewColumn.Two;
-	let panelOptions: vscode.WebviewPanelOptions & vscode.WebviewOptions = {
-		enableScripts: true
-	};
+    let panelTitle: string = "AI Code Reviewer";
+    let panelIdentifier: string = "AI_Code_Reviewer";
+    let panelViewColumnOptions: vscode.ViewColumn = vscode.ViewColumn.Two;
+    let panelOptions: vscode.WebviewPanelOptions & vscode.WebviewOptions = {
+        enableScripts: true
+    };
 
-	let panel: vscode.WebviewPanel = vscode.window.createWebviewPanel(panelTitle,
-		panelIdentifier,
-		panelViewColumnOptions,
-		{
-			enableScripts: true
-		});
+    let panel: vscode.WebviewPanel = vscode.window.createWebviewPanel(panelTitle,
+        panelIdentifier,
+        panelViewColumnOptions,
+        {
+            enableScripts: true
+        });
 
-	const filePath: vscode.Uri = vscode.Uri.file(path.join(context.extensionPath, 'src', 'html', 'index.html'));
-	panel.webview.html = fs.readFileSync(filePath.fsPath, 'utf8');
-	// panel.webview.html = getWebviewHtml();
+    const filePath: vscode.Uri = vscode.Uri.file(path.join(context.extensionPath, 'src', 'html', 'index.html'));
+    panel.webview.html = fs.readFileSync(filePath.fsPath, 'utf8');
+    // panel.webview.html = getWebviewHtml();
 
-	panel.webview.onDidReceiveMessage(async message => {
-		switch (message.command) {
-			case "OnFileSelected":
-				const files = await vscode.window.showOpenDialog({
-					canSelectMany: false,
-					openLabel: 'Select a file',
-					canSelectFiles: true,
-					canSelectFolders: false,
-					defaultUri: vscode.workspace.workspaceFolders?.[0]?.uri
-				});
+    panel.webview.onDidReceiveMessage(async message => {
+        switch (message.command) {
+            case "OnFileSelected":
+                const files = await vscode.window.showOpenDialog({
+                    canSelectMany: false,
+                    openLabel: 'Select a file',
+                    canSelectFiles: true,
+                    canSelectFolders: false,
+                    defaultUri: vscode.workspace.workspaceFolders?.[0]?.uri
+                });
 
-				if (files && files[0]) {
-					const bytes = await vscode.workspace.fs.readFile(files[0]);
-					const content = Buffer.from(bytes).toString('utf8');
+                if (files && files[0]) {
+                    const bytes = await vscode.workspace.fs.readFile(files[0]);
+                    const content = Buffer.from(bytes).toString('utf8');
 
-					panel.webview.postMessage({
-						command: 'DisplayFileContent',
-						content
-					});
-				}
-				break;
-			case "OnSendToChatGPT":
-				console.log("Started Refactoring");
-				let chatReponse = await queryChat(message.content);
-				panel.webview.postMessage({
-					command: 'ReceivedResponseFromChatGPT',
-					content: chatReponse
-				});
-				vscode.window.showInformationMessage("Ended Refactoring");
-				break;
+                    panel.webview.postMessage({
+                        command: 'DisplayFileContent',
+                        content
+                    });
+                }
+                break;
+            case "OnSendToChatGPT":
+                let chatReponse = await queryChat(message.content.fileContent, message.content.userPrompt);
+                panel.webview.postMessage({
+                    command: 'ReceivedResponseFromChatGPT',
+                    content: chatReponse
+                });
+                vscode.window.showInformationMessage("Ended Refactoring");
+                break;
             case "OnModelSelected":
                 let newChatGptModel = message.content.value;
                 setChatGptModel(newChatGptModel);
                 vscode.window.showInformationMessage("New Model Selected: " + newChatGptModel);
                 break;
-		}
-	});
+        }
+    });
 
-	if (fileToLoad) {
-		const bytes = await vscode.workspace.fs.readFile(fileToLoad);
-		const content = Buffer.from(bytes).toString('utf8');
+    if (fileToLoad) {
+        const bytes = await vscode.workspace.fs.readFile(fileToLoad);
+        const content = Buffer.from(bytes).toString('utf8');
 
-		panel.webview.postMessage({
-			command: 'DisplayFileContent',
-			content
-		});
-	}
+        panel.webview.postMessage({
+            command: 'DisplayFileContent',
+            content
+        });
+    }
 }
 
-async function queryChat(content: string): Promise<OpenAI.Responses.Response> {
+async function queryChat(content: string, userPrompt?: string): Promise<OpenAI.Responses.Response> {
 
-	const instructions: string = "You are a code assistant. Refactor and imrpove code and reply with the full result ONLY as JSON is that format {hint:'Hints from response code, some recommendation', code: 'The actual code'}. No comments or explanations. No markdown formatting";
+    vscode.window.showInformationMessage("userPrompt: " + userPrompt);
 
-	const response = await openAIClient.responses.create({
-		model: chatGptModel,
-		instructions: instructions,
-		input: content,
-	});
+    const instructions: string = `You are a code assistant. Refactor and imrpove code and reply with the full result ONLY as JSON is that format {hint:'Hints from response code, some recommendation', code: 'The actual code'}.If a user prompt is provided and it is relevant to the code, take it into account. If it is not related, ignore it.User Prompt: ${userPrompt}. No comments or explanations. No markdown formatting.`;
 
-	return response;
+    const response = await openAIClient.responses.create({
+        model: chatGptModel,
+        instructions: instructions,
+        input: content,
+    });
+
+    return response;
 }
 
 function setChatGptModel(model: string) {
@@ -109,7 +110,7 @@ function setChatGptModel(model: string) {
 }
 
 function getWebviewHtml() {
-	return `<!DOCTYPE html>
+    return `<!DOCTYPE html>
 <html lang="en">
 
 <head>
